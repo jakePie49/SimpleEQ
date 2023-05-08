22fdf1fc960a4f3da971a64f3a42afd411530680
JakePie <jakepie49@gmail.com>
2023-05-08 16:46:31 -0400

    adding parameters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -56,6 +56,8 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    juce::AudioProcessorValueTreeState apvts { *this, nullptr, "Parameters", ParameterLayout,parameterLayout };
+
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
