9835acba71f1b0f9734266ca0d4c6cfbf770271c
JakePie <jakepie49@gmail.com>
2023-05-08 16:47:36 -0400

    adding parameters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -56,7 +56,8 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    juce::AudioProcessorValueTreeState apvts { *this, nullptr, "Parameters", ParameterLayout,parameterLayout };
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    juce::AudioProcessorValueTreeState apvts { *this, nullptr, "Parameters", createParameterLayout()};
 
 private:
     //==============================================================================
